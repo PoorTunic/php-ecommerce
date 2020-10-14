d197b8de1b77f6487a43e5615344047cb760a0ca
Gisela Sandoval <72677834+gisandoval@users.noreply.github.com>
2020-10-13 21:59:57 -0500

    New Control
    Version1.0

--- control.php
+++ control.php
@@ -3,71 +3,85 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <title>Control</title>
-
     <?php require "../php/cdn.php" ?>
 </head>
-
-<body>
-<?php require "../php/header.php" ?>
-
-<div class="container">
-            <nav class="text-white navbar navbar-expand navbar-toggler" style="background-color: black; color: white; width: 100%">
-
-                <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNav">
-                    <div>
-                        <ul class="navbar-nav">
-                            <li class="nav-item" class="col-sm-1 col-md-2" style="text-align: right;">
-                            <h3><?php echo $_SESSION["username"]; ?></h3>
-                            </li>
-                            <li class="nav-item" class="col-sm-1 col-md-2" style="margin-left: 10px">
-                                <button type="button" class="btn btn-dark" style="font-size: 20px">Salir</button>
-                            </li>
-
-                        </ul>
-                    </div>
+<body style="background-color: #F5F5F5">
+    <?php require "../php/header.php" ?>
+    <div>
+        <nav class="navbar navbar-dark bg-dark text-white">
+            <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item mb-3 h3">
+                        <font face="Darker Grotesque">
+                            <?php echo $_SESSION["username"];?>
+                            <button type="button" class="btn btn-dark">
+                            <h3>Salir</h3>
+                            </button>
+                        </font>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <div>
+        <div class="row">
+            <div class="col-sm">
+                <font face="Darker Grotesque">
+                <div class="btn-group-vertical bg-dark" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-secondary"><h3>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house-door-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/>
+                    <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                    </svg> Inicio</h3></button>
+                    <button type="button" class="btn btn-secondary"><h3>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-people-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+                    </svg> Administradores</h3></button>
+                    <button type="button" class="btn btn-secondary"><h3>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-people" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1h7.956a.274.274 0 0 0 .014-.002l.008-.002c-.002-.264-.167-1.03-.76-1.72C13.688 10.629 12.718 10 11 10c-1.717 0-2.687.63-3.24 1.276-.593.69-.759 1.457-.76 1.72a1.05 1.05 0 0 0 .022.004zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10c-1.668.02-2.615.64-3.16 1.276C1.163 11.97 1 12.739 1 13h3c0-1.045.323-2.086.92-3zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+                    </svg> Clientes</h3></button>
+                    <button type="button" class="btn btn-secondary"><h3>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-diagram-3-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5v-1zm-6 8A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5v-1zm6 0A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5v-1zm6 0a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1z"/>
+                    </svg> Categorías</h3></button>
+                    <button type="button" class="btn btn-secondary"><h3>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-lines-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7 1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm2 9a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+                    </svg> Proveedores</h3></button>
+                    <button type="button" class="btn btn-secondary"><h3>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-box-seam" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+                    </svg> Productos</h3></button>
+                    <button type="button" class="btn btn-secondary"><h3>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                    </svg> Compras</h3></button>
+                    <button type="button" class="btn btn-secondary"><h3>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5A.5.5 0 0 0 4 8z"/>
+                    </svg> Slider</h3></button>
+                    <button type="button" class="btn btn-secondary"><h3>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-box-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                    </svg> Salir</h3></button>
                 </div>
-            </nav>
-
-
-
-            <div class="row">
-
-
-
-                <div class="col-sm-12 col-md-4 mx-auto">
-                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-                        <div class="btn-group-vertical" role="group" aria-label="First group">
-                            <button type="button" class="btn btn-primary btn-block p-2 bg-dark text-white fa fa-home" style="max-width:100%"> Inicio</button>
-                            <button type="button" class="btn btn-primary btn-block p-2 bg-dark text-white fa fa-user" style="max-width:100%"> Administradores</button>
-                            <button type="button" class="btn btn-primary btn-block p-2 bg-dark text-white fa fa-user-o" style="max-width:100%">  Clientes</button>
-                            <button type="button" class="btn btn-primary btn-block p-2 bg-dark text-white fa fa-bars" style="max-width:100%">   Categorías</button>
-                            <button type="button" class="btn btn-primary btn-block p-2 bg-dark text-white fa fa-users" style="max-width:100%">  Proveedores</button>
-                            <button type="button" class="btn btn-primary btn-block p-2 bg-dark text-white fa fa-laptop" style="max-width:100%"> Productos</button>
-                            <button type="button" class="btn btn-primary btn-block p-2 bg-dark text-white fa fa-shopping-cart" style="max-width:100%"> Compras</button>
-                            <button type="button" class="btn btn-primary btn-block p-2 bg-dark text-white fa fa-arrow-right" style="max-width:100%"> Slider</button>
-                            <button type="button" class="btn btn-primary btn-block p-2 bg-dark text-white fa fa-sign-out" style="max-width:100%"> Salir</button>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-8 mx-auto">
-                    <br><br>
-                    
-                    <h1 class="display-4">BIENVENIDO AL SISTEMA</h1>
-
-                </div>
-
-
+                </font>
+            </div>
+            <div class="col-sm">
+                <font face="Darker Grotesque" size="6">
+                    Bienvenido al sistema
+                </font>
             </div>
         </div>
-
+    </div>
     <?php require "../php/footer.php" ?>
 </body>
-
+</head>
 </html>
